9ee2311994d050eec4dff44658ee207c0ec8a8f5
Johannes <jogus@live.de>
2016-04-13 18:51:23 +0200

    GradVer

--- GradVer.v
+++ GradVer.v
@@ -13,16 +13,68 @@ Require Import Coq.Logic.ProofIrrelevance.
 
 (* Figure 1: Syntax of a Java-like language for core language *)
 Definition C := string.
+Definition x := string.
+Definition f := string.
+Definition m := string.
+Definition o := nat.
 Inductive T :=
 | TPrimitiveInt : T
 | TClass : C -> T.
-
-Definition S := list ()
-
+Inductive v :=
+| vn : nat -> v
+| vnull : v
+| vo : o -> v.
+Inductive e :=
+| ev : v -> e
+| ex : x -> e
+| ethis : e
+| eresult : e
+| edot : e -> f -> e.
+Inductive phi :=
+| phiTrue : phi
+| phiEq : e -> e -> phi
+| phiNeq : e -> e -> phi
+| phiAcc : x -> f -> phi
+| phiConj : phi -> phi -> phi
+| phiAssert : x -> T -> phi.
+Inductive s :=
+| sMemberSet : x -> f -> x -> s
+| sDeclare : T -> x -> s
+| sSet : x -> e -> s
+| sAlloc : x -> C -> s
+| sCall : x -> x -> m -> list x -> s
+| sReturn : x -> s
+| sAssert : phi -> s
+| sRelease : phi -> s.
+Inductive contract :=
+| Contract : phi -> phi -> contract.
+Inductive method :=
+| Method : T -> m -> list (T * x) -> contract -> list s -> method.
+Inductive field :=
+| Field : T -> f -> field.
+Inductive cls :=
+Cls : C -> list field -> list method -> cls.
 Inductive program :=
-| Program : (list cls) -> (list s).
+| Program : (list cls) -> (list s) -> program.
 
+Definition H := o -> C * (list (f * v)).
+Definition rho := list (x * v).
+Inductive name :=
+| namex : x -> name
+| nameo : o -> name.
+Definition A := list (name * f).
+Definition S := list (rho * A * list s).
 
-Definition phi (x : nat) := x.
-
+(* notation *)
 Notation "'φ'" := phi.
+Notation "'ρ'" := rho.
+
+(* Figure 2: Static typing rules for expressions of the core language *)
+Inductive sfrme : A -> e -> Prop :=
+| WFVar : forall a (x' : x), sfrme a (ex x')
+| WFValue : forall a (v' : v), sfrme a (ev v')
+| WFField : forall a (x' : x) (f' : f), sfrme a (edot (ex x') f')
+.
+
+(* Figure 3: Static rules for syntactically self-framed formulas *)
+Inductive sfrmphi : 
