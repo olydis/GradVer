4913de7d06ab5780dcdd9a471673e8354db4b29f
Johannes <jogus@live.de>
2016-05-27 19:04:50 -0400

    ...

--- GradVerProofs.v
+++ GradVerProofs.v
@@ -636,10 +636,31 @@ Definition A_sSubsts m (A : A_s) : A_s :=
 Definition FVA_s (A : A_s) : list x := map fst A.
 
 Definition FVmTarg (m : list (x * e)) : list x := flat_map FVe (map snd m).
-(*
+
+Check in_flat_map.
+
+Fixpoint FVeo (e : e) : option x := 
+  match e with
+  | ev _ => None
+  | ex x => Some x
+  | edot e _ => FVeo e
+  end.
+
+Definition mMapsTo (m : list (x * e)) (x' : x) : Prop :=
+  exists m', In m' m /\ FVeo (snd m') = Some x'.
+
+Definition mMapsToUnique (m : list (x * e)) (x' : x) : Prop :=
+  forall f1 f2 e1 e2, 
+      In (f1, e1) m -> 
+      In (f2, e2) m -> 
+      FVeo e1 = Some x' -> 
+      FVeo e2 = Some x' -> 
+      f1 = f2
+.
+
 (* sfrme (A_sSubsts (a => b) (a.f)) (eSubsts (a => b) (b.f)) -> sfrme (a.f) (b.f) *)
 Lemma sfrmeSubst : forall m e a,
-      (forall x, In x (FVe e) -> ~ In x (FVmTarg m)) ->
+      (forall x, mMapsToUnique m x /\ (mMapsTo m x -> (~ In x (FVe e) /\ ~ In x (FVA_s a)))) ->
       sfrme (A_sSubsts m a) (eSubsts m e) ->
       sfrme a e.
 Proof.
@@ -651,16 +672,6 @@ Proof.
   inversionx H1.
   destruct e0; simpl in H0; inversionx H2.
   constructor.
-  specialize (H0 x1). intuition.
-  clear H2.
-  (*unfold FVmTarg in H0.
-  rewrite in_flat_map in H0.
-  assert (forall x0 : e, ~ In x0 (map snd m0) \/ ~ In x1 (FVe x0)).
-    intros.
-    eapply not_ex_all_not in H0.
-    eapply not_and_or in H0.
-    eauto.
-    clear H0.*)
   destruct (find (λ r : x * e, x_decb x1 (fst r)) m0) eqn: ff.
   - destruct p0.
     subst.
@@ -690,175 +701,75 @@ Proof.
       inversionx fff1.
       unfold x_decb, dec2decb in *. simpl in *.
       des (x_dec x3 x4); inversionx H3.
-
-(* sfrme (A_sSubsts (x4 => x0, x2 => x0) (x4.f0)) (eSubsts (x4 => x0, x2 => x0) (x2.f0)) -> 
-   sfrme (x4.f0) (x2.f0) *)
-
-
-      assert (In x3 (FVA_s a)).
-        unfold FVA_s. apply in_map_iff. eexists; split; eauto; simpl; tauto.
-      apply H2 in H1. intuition. inversionx H3.
+      specialize (H0 x0).
+      inversionx H0.
+      unfold mMapsToUnique in H3.
+      eapply H3 in H1; eauto.
+      subst.
       assumption.
-    * assert (In x0 (FVA_s a)).
-        unfold FVA_s. apply in_map_iff. eexists; split; eauto; simpl; tauto.
-      apply H2 in H1. intuition. inversionx H3.
-      assumption.
+    * specialize (H0 x0).
+      inversionx H0.
+      assert (mMapsTo m0 x0).
+        eexists; eauto.
+      intuition.
+      unfold FVA_s in H6.
+      rewrite in_map_iff in H6.
+      contradict H6.
+      eexists; split; eauto.
+      auto.
   - inversionx H3.
-    econstructor.
     unfold A_sSubsts in H4.
     apply in_flat_map in H4.
     inversionx H4.
-    inversionx H0.
-    destruct x0; inversionx H3; try inversionx H0.
-    unfold phiSubsts in H1.
-    apply in_map_iff in H1.
     inversionx H1.
-    inversionx H0.
-    unfold phiSubsts, phi'Substs in H1.
-    destruct x0; inversionx H1.
+    destruct x0; inversionx H3; try inversionx H1.
+    unfold phiSubsts in H2.
+    apply in_map_iff in H2.
+    inversionx H2.
+    inversionx H1.
+    unfold phiSubsts, phi'Substs in H2.
+    destruct x0; inversionx H2.
     * destruct (find (λ r : x * e, x_decb x0 (fst r)) m0) eqn: ff1.
       + destruct p0.
         destruct e0; inversionx H4.
         apply find_some in ff1.
         inversionx ff1.
-        unfold x_decb, dec2decb in H1.
-        simpl in H1.
-        des (x_dec x0 x2); inversionx H1.
+        unfold x_decb, dec2decb in H2.
+        simpl in H2.
+        des (x_dec x0 x2); inversionx H2.
         apply in_map_iff in H3.
         inversionx H3.
-        inversionx H1. 
+        inversionx H2.
         inversionx H3.
         destruct x3.
         simpl in *.
-        unfold incl in H2.
-        specialize (H2 x2).
-        assert (In x2 (FVA_s a)).
-          unfold FVA_s. apply in_map_iff. eexists; split; eauto; simpl; tauto.
+        specialize (H0 x1).
+        inversionx H0.
+        assert (mMapsTo m0 x1).
+          eexists; eauto.
         intuition.
-        inversionx H3; try inversionx H2.
-        assumption.
       + inversionx H4.
         apply in_map_iff in H3.
         inversionx H3.
-        inversionx H0. 
-        inversionx H1.
+        inversionx H1. 
+        inversionx H2.
         destruct x0.
         simpl.
         assumption.
     * apply in_map_iff in H3.
       inversionx H3.
-      inversionx H0.
-      inversion H1.
-Qed.*)
-
-Lemma sfrmeSubst : forall m e a,
-      (sfrme (A_sSubsts m a) (eSubsts m e) /\ incl (FVA_s a) (FVe e))
-      ->
-      sfrme a e.
-Proof.
-  induction e0;
-  intros; try constructor;
-  simpl in *;
-  try apply inclEmpty;
-  try constructor.
-  inversionx H0.
-  inversionx H1.
-  destruct e0; simpl in H0; inversionx H0.
-  destruct (find (λ r : x * e, x_decb x1 (fst r)) m0) eqn: ff.
-  - destruct p0.
-    subst.
-    assert (fff := ff).
-    apply find_some in ff.
-    inversionx ff.
-    unfold x_decb, dec2decb in H1.
-    des (x_dec x1 (fst (x2, ex x0))); try inversionx H1.
-    simpl.
-    econstructor.
-    unfold A_sSubsts in H4.
-    apply in_flat_map in H4.
-    inversionx H4.
-    inversionx H1.
-    destruct x3; inversionx H4.
-    * inversionx H1.
-      simpl in *.
-      unfold incl in H2.
-      unfold phiSubsts, phi'Substs in H3.
-      simpl in *.
-      apply in_map_iff in H3.
-      inversionx H3.
       inversionx H1.
-      apply in_map_iff in H4.
-      inversionE H4.
-      inversionx H1.
-      destruct x4.
-      simpl in *.
-      destruct (find (λ r : x * e, x_decb x3 (fst r)) m0) eqn: fff1; inversionx H3.
-      + destruct p0. destruct e0; inversionx H4.
-        assert (In x3 (FVA_s a)).
-          unfold FVA_s. apply in_map_iff. eexists; split; eauto; simpl; tauto.
-        apply H2 in H1. intuition. inversionx H3.
-        assumption.
-      + assert (In x0 (FVA_s a)).
-          unfold FVA_s. apply in_map_iff. eexists; split; eauto; simpl; tauto.
-        apply H2 in H1. intuition. inversionx H3.
-        assumption.
-    * inversion H1.
-  - inversionx H3.
-    econstructor.
-    unfold A_sSubsts in H4.
-    apply in_flat_map in H4.
-    inversionx H4.
-    inversionx H0.
-    destruct x0; inversionx H3; try inversionx H0.
-    unfold phiSubsts in H1.
-    apply in_map_iff in H1.
-    inversionx H1.
-    inversionx H0.
-    unfold phiSubsts, phi'Substs in H1.
-    destruct x0; inversionx H1.
-    * destruct (find (λ r : x * e, x_decb x0 (fst r)) m0) eqn: ff1.
-      + destruct p0.
-        destruct e0; inversionx H4.
-        apply find_some in ff1.
-        inversionx ff1.
-        unfold x_decb, dec2decb in H1.
-        simpl in H1.
-        des (x_dec x0 x2); inversionx H1.
-        apply in_map_iff in H3.
-        inversionx H3.
-        inversionx H1. 
-        inversionx H3.
-        destruct x3.
-        simpl in *.
-        unfold incl in H2.
-        specialize (H2 x2).
-        assert (In x2 (FVA_s a)).
-          unfold FVA_s. apply in_map_iff. eexists; split; eauto; simpl; tauto.
-        intuition.
-        inversionx H3; try inversionx H2.
-        assumption.
-      + inversionx H4.
-        apply in_map_iff in H3.
-        inversionx H3.
-        inversionx H0. 
-        inversionx H1.
-        destruct x0.
-        simpl.
-        assumption.
-    * apply in_map_iff in H3.
-      inversionx H3.
-      inversionx H0.
-      inversion H1.
+      inversionx H2.
 Qed.
 
 Lemma sfrmeSubstEmpty : forall m e,
       sfrme [] (eSubsts m e) -> sfrme [] e.
 Proof.
   intros.
-  eapply sfrmeSubst.
-  split; eauto.
-  simpl.
-  apply inclEmpty.
+  destruct e0; try constructor.
+  simpl in *.
+  inversionx H0.
+  inversion H3.
 Qed.
 
 Lemma eSubstsEmpty : forall p, eSubsts [] p = p.
@@ -876,28 +787,53 @@ Proof.
 Qed.
 
 Lemma sfrmphi'Subst : forall m e a,
-      (sfrmphi' (A_sSubsts m a) (phi'Substs m e) /\ incl (FVA_s a) (FV' e))
+     (forall x, mMapsToUnique m x /\ (mMapsTo m x -> (~ In x (FV' e) /\ ~ In x (FVA_s a)))) ->
+      sfrmphi' (A_sSubsts m a) (phi'Substs m e)
       ->
       sfrmphi' a e.
 Proof.
   intros.
-  inversionx H0.
   destruct e0; constructor;
   inversionx H1;
-  apply (sfrmeSubst m0); intuition.
-Admitted.
+  apply (sfrmeSubst m0); intuition;
+  try apply H0;
+  apply H0 in H1;
+  inversionx H1;
+  intuition;
+  contradict H3;
+  simpl;
+  intuition.
+Qed.
 
-(* counter-example
+(* counter-examples:
 sfrmphi [] (phiSubsts (a => c, b => c) (acc(b.f) * a.f = 3)) ->
+sfrmphi [] (acc(b.f) * a.f = 3)
+
+sfrmphi [] (phiSubsts (a => b) (acc(b.f) * a.f = 3)) ->
+sfrmphi [] (acc(b.f) * a.f = 3)
+
+sfrmphi [] (phiSubsts (b => a) (acc(b.f) * a.f = 3)) ->
 sfrmphi [] (acc(b.f) * a.f = 3)
 *)
 
-Lemma sfrmphiSubst : forall p m, sfrmphi [] (phiSubsts m p) -> sfrmphi [] p.
+Lemma sfrmphiSubst : forall e m a,
+     (forall x, mMapsToUnique m x /\ (mMapsTo m x -> (~ In x (FV e) /\ ~ In x (FVA_s a)))) ->
+      sfrmphi (A_sSubsts m a) (phiSubsts m e)
+      ->
+      sfrmphi a e.
 Proof.
-  induction p0; intros; constructor.
-  - inversionx H0.
-    eapply sfrmphi'Subst. split; eauto.
-    simpl. apply inclEmpty.
+  induction e0; intros; constructor.
+  - inversionx H1.
+    eapply sfrmphi'Subst; eauto.
+    intros.
+    split; try apply H0.
+    intros.
+    apply H0 in H1.
+    inversionx H1.
+    intuition.
+    contradict H4.
+    simpl.
+    intuition.
   - destruct a; simpl in *; intuition;
     try (apply IHp0 in H2); try assumption.
     * destruct (find (λ r : x * e, x_decb x0 (fst r)) m0) eqn: ff.
