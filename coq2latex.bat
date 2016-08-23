f279c9b6154519fbc4af7a81ec6ff440746a1273
Johannes <jogus@live.de>
2016-08-23 11:56:57 -0400

    PDF restruct

--- coq2latex.bat
+++ coq2latex.bat
@@ -3,26 +3,26 @@
 ((echo Set Printing Depth 2. Load GradVer0Defs. Set Printing All. Set Printing Width 2097151. Set Printing Depth 100. Print Semantics.sfrme. Quit.^
     | C:\Users\Johannes\AppData\Roaming\Coq\bin\coqtop.exe) && type GradVer0Defs.v)^
     | ..\coq2latex\project\coq2latex\bin\Debug\coq2latex.exe Semantics.sfrme^
-    > latex\staticExpression.tex
+    > ..\GradVerThesis\data\autogen\staticExpression.tex
 
 ((echo Set Printing Depth 2. Load GradVer0Defs. Set Printing All. Set Printing Width 2097151. Set Printing Depth 100. Print Semantics.sfrmphi'. Quit.^
     | C:\Users\Johannes\AppData\Roaming\Coq\bin\coqtop.exe) && type GradVer0Defs.v)^
     | ..\coq2latex\project\coq2latex\bin\Debug\coq2latex.exe Semantics.sfrmphi'^
-    > latex\staticFormula.tex
+    > ..\GradVerThesis\data\autogen\staticFormula.tex
 
 ((echo Set Printing Depth 2. Load GradVer0Defs. Set Printing All. Set Printing Width 2097151. Set Printing Depth 100. Print Semantics.hoare. Quit.^
     | C:\Users\Johannes\AppData\Roaming\Coq\bin\coqtop.exe) && type GradVer0Defs.v)^
     | ..\coq2latex\project\coq2latex\bin\Debug\coq2latex.exe Semantics.hoare^
-    > latex\staticSemantics.tex
+    > ..\GradVerThesis\data\autogen\staticSemantics.tex
 
 ((echo Set Printing Depth 2. Load GradVer0Defs. Set Printing All. Set Printing Width 2097151. Set Printing Depth 100. Print Semantics.evalphi'. Quit.^
     | C:\Users\Johannes\AppData\Roaming\Coq\bin\coqtop.exe) && type GradVer0Defs.v)^
     | ..\coq2latex\project\coq2latex\bin\Debug\coq2latex.exe Semantics.evalphi'^
-    > latex\dynamicFormula.tex
+    > ..\GradVerThesis\data\autogen\dynamicFormula.tex
 
 ((echo Set Printing Depth 2. Load GradVer0Defs. Set Printing All. Set Printing Width 2097151. Set Printing Depth 100. Print Semantics.dynSem. Quit.^
     | C:\Users\Johannes\AppData\Roaming\Coq\bin\coqtop.exe) && type GradVer0Defs.v)^
     | ..\coq2latex\project\coq2latex\bin\Debug\coq2latex.exe Semantics.dynSem^
-    > latex\dynamicSemanticsX.tex
+    > ..\GradVerThesis\data\autogen\dynamicSemantics.tex
 
 GOTO:EOF
